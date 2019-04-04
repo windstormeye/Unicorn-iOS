76fa9b5efca0640fda52529b0d013df4ac4c38ad
windstormeye <877302410@qq.com>
2019-04-04 10:50:12 +0800

    [v0.0.1] <init> UNBottomView

--- Unicorn/Component/Text/UNTextViewController.swift
+++ Unicorn/Component/Text/UNTextViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 
 class UNTextViewController: UIViewController {
 
+    private var textView = UITextView()
+    private var bottomView = UNBottomView()
+    
+    private let topViewBottom = topSafeAreaHeight + 10 + 30
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initView()
     }
     
     private func initView() {
-        let backButton = UIButton(frame: CGRect(x: 15, y: topSafeAreaHeight + 10, width: 30, height: 30))
+        let backButton = UIButton(frame: CGRect(x: 15, y: statusBarHeight + 10, width: 30, height: 30))
         view.addSubview(backButton)
         backButton.titleLabel?.font = UIFont.systemFont(ofSize: 25)
         backButton.setTitle("←", for: .normal)
@@ -25,5 +30,45 @@ class UNTextViewController: UIViewController {
         view.addSubview(doneButton)
         doneButton.titleLabel?.font = backButton.titleLabel?.font
         doneButton.setTitle("✓", for: .normal)
+        
+        textView.frame = CGRect(x: 15, y: backButton.bottom + 10, width: view.width - 30, height: 300)
+        textView.backgroundColor = .clear
+        textView.textAlignment = .center
+        textView.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        textView.becomeFirstResponder()
+        textView.tintColor = .white
+        textView.textColor = .white
+        textView.keyboardAppearance = .dark
+        view.addSubview(textView)
+        
+        bottomView.frame = CGRect(x: 0, y: view.height - bottomSafeAreaHeight - 64, width: view.width, height: 64)
+        view.addSubview(bottomView)
+        bottomView.itemImageNames = ["2", "2", "2", "2"]
+        
+        NotificationCenter.default.addObserver(self, selector: .keyboardFrame, name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     }
+    
+    @objc
+    fileprivate func keyBoardFrameChange(_ notification: Notification){
+        let info = notification.userInfo
+        let kbRect = (info?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let offsetY = kbRect.origin.y - view.height
+        print(offsetY)
+        UIView.animate(withDuration: 0.3) {
+            if offsetY == 0 {
+                self.bottomView.transform = CGAffineTransform(translationX: 0, y: 0)
+            }else{
+                self.bottomView.transform = CGAffineTransform(translationX: 0, y: offsetY)
+                self.textView.height = self.view.height - abs(offsetY) - self.topViewBottom - 45 - self.bottomView.height
+            }
+        }
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
+
+private extension Selector {
+    static let keyboardFrame = #selector(UNTextViewController.keyBoardFrameChange(_:))
 }
